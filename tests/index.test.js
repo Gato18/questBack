cc4c062d400a87f18d8c9f66d8c9301151aa6f0b
Gato18 <g.canonge@gmail.com>
2021-11-16 17:44:15 +0100

    add some tdd

---
+++ tests/index.test.js
@@ -0,0 +1,36 @@
+var app = require("../app");
+var request = require("supertest");
+const mongoose = require("mongoose");
+
+test("check signIn detail- Right data", async () => {
+  await request(app)
+    .post("/users/sign-in")
+    .send({ emailFromFront: "nico@gmail.com", passwordFromFront: "007" })
+    .expect(200)
+    .then((response) => {
+      expect(response.body.dataUser.token).toBe("5b8GdVtMdk9H5yYUDr9acVpM7qyoCOD3");
+    });
+});
+
+test("get userDetail test- Wrong token", async () => {
+  await request(app)
+    .get("/home/userDetail")
+    .query({ token: "badToken" })
+    .then((response) => {
+      expect(response.body.result).toBe(false);
+    });
+});
+
+test("get userDetail test- Correct token", async () => {
+  await request(app)
+    .get("/home/userDetail")
+    .query({ token: "mCwTMKjgkxTo7BFeAoio_Omi3KW_KTTx" })
+    .expect(200)
+    .then((response) => {
+      expect(response.body.result).toBe(true);
+    });
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
